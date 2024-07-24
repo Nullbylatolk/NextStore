f2d569861ebc8668e4319386e63b48b54b7cd3b6
Nullbylatolk <tlatolk.aguilar@gmail.com>
2024-07-23 19:03:54 -0600

    [add] Main products [Fix] getProducts

--- src/services/shopify/products.ts
+++ src/services/shopify/products.ts
@@ -1,21 +1,45 @@
 import { shopifyUrls } from "./urls";
 import { env } from "app/config/env";
 
-
-export const getProducts = async () => {
+export const getProducts = async (id?: string): Promise<ProductType[]> => {
   try {
-    const response = await fetch(shopifyUrls.products.all,
-      {
-        headers: new Headers({
-          "X-Shopify-Access-Token": env.SHOPIFY_TOKEN,
-        }),
-      }
-    );
+    const apiUrl = id
+      ? `${shopifyUrls.products.all}?ids=${id}`
+      : shopifyUrls.products.all;
+
+    const response = await fetch(apiUrl, {
+      headers: new Headers({
+        "X-Shopify-Access-Token": env.SHOPIFY_TOKEN,
+      }),
+    });
     const { products } = await response.json();
-    return products;
+
+    const transformedProducts = products.map((item: any) => {
+      return {
+        id: item.id,
+        gql_id: item.variants[0].admin_graphql_api_id,
+        title: item.title,
+        description: item.body_html,
+        price: item.variants[0].price,
+        image: item.images[0].src,
+        quantity: item.variants[0].inventory_quantity,
+        handle: item.handle,
+        tags: item.tags,
+      };
+    });
+    return transformedProducts;
   } catch (error) {
     console.log(error);
   }
 };
 
 
+export const getMainProducts = async () =>{
+  const response = await fetch(shopifyUrls.products.main,{
+      headers: new Headers({
+        "X-Shopify-Access-Token": env.SHOPIFY_TOKEN,
+      }),
+  })
+  const {products} = await response.json()
+  return products
+}
--- src/services/shopify/urls.ts
+++ src/services/shopify/urls.ts
@@ -3,6 +3,7 @@ import { env } from "app/config/env";
 export const shopifyUrls = {
   products: {
     all: `${env.SHOPIFY_HOSTNAME}/admin/api/2024-07/products.json`,
+    main:`${env.SHOPIFY_HOSTNAME}/admin/api/2024-07/collections/318667358365/products.json`
   },
   collections: {
     all: `${env.SHOPIFY_HOSTNAME}/admin/api/2024-07/smart_collections.json`,
