d81af9f9b57572d7059745e5e678dcf8f5eb2020
Nullbylatolk <tlatolk.aguilar@gmail.com>
2024-07-25 11:52:08 -0600

    [Mod] Retornamos el acces Token'

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -31,3 +31,12 @@ export const handleCreateUser = async (formData: FormData) => {
 
 
 };
+
+
+export const handleLogin = async (formData:FormData)=>{
+  const formDataObject = Object.fromEntries(formData);
+  const accesToken = await createAccessToken(formDataObject.email as string, formDataObject.password as string);
+  if(accesToken){
+    redirect('/store')
+  }
+}
