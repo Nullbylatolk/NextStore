573816fc6edffab47462e75ae9b7c9646460477c
Nullbylatolk <tlatolk.aguilar@gmail.com>
2024-07-25 10:26:21 -0600

    [Add] Auth, CreateAcces Token, save Cookie, Graphql

---
+++ src/utils/auth/createAccesToken.ts
@@ -0,0 +1,27 @@
+import { GraphQLClientSingleton } from "app/graphql";
+import { customerAccessTokenCreateMutation } from "app/graphql/mutations/customerAccesTokenCreate";
+import { cookies } from "next/headers";
+
+export const createAccessToken = async (email: string, password: string) => {
+  const cookiesStore = cookies();
+  const graphqlClient = GraphQLClientSingleton.getInstance().getClient();
+  const { customerAccessTokenCreate } = await graphqlClient.request(
+    customerAccessTokenCreateMutation,
+    {
+      email: email,
+      password: password,
+    }
+  );
+
+  const { accessToken, expiresAt } =
+  customerAccessTokenCreate?.customerAccessToken;
+  console.log("accesToken",accessToken)
+  if (accessToken) {
+    cookiesStore.set("accesToken", accessToken, {
+      path: "/",
+      expires: new Date(expiresAt),
+      httpOnly: true,
+      sameSite: "strict",
+    });
+  }
+};
