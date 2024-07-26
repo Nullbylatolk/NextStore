8a46c890f3e7364acd72e9e15bdeb49ef11d5223
Nullbylatolk <tlatolk.aguilar@gmail.com>
2024-07-26 10:48:58 -0600

    [Add] Items al carrito de compras

--- src/components/shared/ShoppingCart/ShoppingCart.tsx
+++ src/components/shared/ShoppingCart/ShoppingCart.tsx
@@ -1,15 +1,38 @@
 "use client"
-import { useState } from "react";
+
 import { FaShoppingCart } from "react-icons/fa";
 import styles from './ShoppingCart.module.sass'
-
+import { useShoppingCart } from "app/hooks/useShoppingCart";
+import { useState } from "react";
 
 export const ShoppingCart = () => {
-    const [countItems, setCountItems] = useState(0);
+
+    const [isOpen, setIsOpen] = useState(false)
+
+
+    const handleOpen = () => setIsOpen(!isOpen);
+
+    const { cart } = useShoppingCart();
     return (
-        <button className={styles.ShoppingCart}>
-            <span className={styles.ShoppingCart__counter}>{countItems}</span>
+        <button className={styles.ShoppingCart} onClick={handleOpen}>
+            <span className={styles.ShoppingCart__counter}>{cart.length}</span>
             <FaShoppingCart />
+            {isOpen && (
+                <div className={styles.ShoppingCart__items}>
+                    {
+                        cart.map(item => (
+                            <>
+                                <p key={item.id}>{item?.title}  </p>
+                                <p>Cantidad: {item?.quantity}</p>
+                            </>
+                        ))
+                    }
+                    <button className={styles.ShoppingCart__button}>
+                        Buy
+                    </button>
+                </div>
+            )}
+
         </button>
     )
 };
