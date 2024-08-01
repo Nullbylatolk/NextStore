05366ea54cf126fa57ffbba54583160668f94cdc
Nullbylatolk <tlatolk.aguilar@gmail.com>
2024-08-01 16:48:03 -0600

    test

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -4,7 +4,7 @@ export const Hero = () =>{
 
     return (
         <section className={styles.Hero}>
-            <h1>Future World</h1>
+            <h1>Future World by Tlatolk</h1>
             <h2>Empowering Your Tomorrow, Today!</h2>
         </section>
     );
